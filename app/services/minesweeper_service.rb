a7cd95d4f1ed58427cb2049fb9882336b2416e9a
Agustin Marmolejo <eljamz@gmail.com>
2024-01-26 19:32:13 -0600

    feat: ensure that we only have one mine in an specific spot

--- app/services/minesweeper_service.rb
+++ app/services/minesweeper_service.rb
@@ -1,13 +1,20 @@
 class MinesweeperService
   def self.generate_board(width, height, mines_count)
-    # Create a blank board
+    # Create a blank board (2 dimensional array)
     board = Array.new(height) { Array.new(width, 0) }
 
-    # Randomly place mines
-    mines_count.times do
+    # Start the track for placed mines
+    placed_mines = 0
+
+    while placed_mines < mines_count
       x = rand(width)
       y = rand(height)
-      board[y][x] = 'X' # 'X' represents a mine
+
+      # Place a mine only if there isn't one already
+      if (board[y][x]).zero?
+        board[y][x] = 'X' # 'X' represents a mine
+        placed_mines += 1
+      end
     end
 
     # Return the board
